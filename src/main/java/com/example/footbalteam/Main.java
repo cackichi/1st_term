73642b7c7b354f91310982c1c37410641d7c8585
cackichi <21010022@study.bsuir.by>
2023-05-02 11:30:35 +0300

    m

--- src/main/java/com/example/footbalteam/Main.java
+++ src/main/java/com/example/footbalteam/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
         stage.setTitle("Авторизация");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
