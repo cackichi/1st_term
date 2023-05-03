3d8aff376721d4c19de524488ed4d201f65fd25c
cackichi <21010022@study.bsuir.by>
2023-05-03 16:41:22 +0300

    +file_exists

--- src/main/java/com/example/footbalteam/Main.java
+++ src/main/java/com/example/footbalteam/Main.java
@@ -4,19 +4,26 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
+
+import java.io.*;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("auth.fxml"));
+
+        File file = new File("E:\\FootballTeam\\FootbalTeam\\src\\main\\java\\com\\example\\footbalteam\\accounts.csv");
+        if(file.createNewFile()) {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            bw.write("admin;admin");
+            bw.close();
+        }
+
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setResizable(false);
         stage.setTitle("Авторизация");
         stage.setScene(scene);
         stage.show();
-
-
     }
 
     public static void main(String[] args) {
